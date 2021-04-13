56931085f762f768c1f3dc09413be4a37429d11a
Damian Lago <damianlagomail@gmail.com>
2021-04-13 16:01:46 +0200

    done

---
+++ main.c
@@ -0,0 +1,20 @@
+#include "get_next_line.h"
+#include <stdio.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+int main(void)
+{
+	int fd;
+	char *line;
+	int i = 1;
+	fd = open("test.txt", O_RDONLY);
+	while (get_next_line(fd, &line))
+	{
+		printf("%s", line);
+		free(line);
+		line = NULL;
+	}
+	//system("leaks a.out");
+	return (0);
+}
