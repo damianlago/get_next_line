e73a1a86df425990032945fd04085ca49c21a937
Damian Lago <damianlagomail@gmail.com>
2021-04-13 16:00:37 +0200

    done

---
+++ get_next_line.c
@@ -0,0 +1,64 @@
+#include "get_next_line.h"
+
+void	ft_freestr(char **str)
+{
+	if (str)
+	{
+		free(*str);
+		*str = NULL;
+	}
+}
+
+int	ft_return(int fd, char **line, int b_read, char **str)
+{
+	int	len;
+
+	len = 0;
+	if (b_read == 0 && str[fd] == NULL)
+	{
+		*line = ft_strdup("");
+		return (0);
+	}
+	while (str[fd][len] != '\0' || str[fd][len] != '\n')
+		len++;
+	if (str[fd][len] == '\n')
+	{
+		*line = ft_substr(str[fd], 0, len);
+		str[fd] = ft_strdup(&str[fd][len + 1]);
+		if (str[fd][0] == '\0')
+			ft_freestr(&str[fd]);
+	}
+	else
+	{
+		*line = ft_strdup(str[fd]);
+		ft_freestr(&str[fd]);
+		return (0);
+	}
+	return (1);
+}
+
+int	get_next_line(int fd, char **line)
+{
+	static char	*str[4096];
+	char		buff[BUFFER_SIZE + 1];
+	char		*temp;
+	int		b_read;
+
+	if (fd < 0 || line == NULL || BUFFER_SIZE < 1)
+		return (-1);
+	while ((b_read = read(fd, buff, BUFFER_SIZE)) > 0)
+	{
+		buff[b_read] = '\0';
+		if (str[fd] == NULL)
+			str[fd] = ft_substr("", 0, 0);
+		temp = ft_strjoin(str[fd], buff);
+		free(str[fd]);
+		str[fd] = temp;
+		if (ft_strchr(buff, '\n'))
+			break;
+	}
+	if (b_read < 0)
+		return (-1);
+	else
+		return (ft_return(fd, line, b_read, str));
+}
---
+++ get_next_line_utils.c
@@ -0,0 +1,86 @@
+#include "get_next_line.h"
+
+size_t	ft_strlen(const char *str)
+{
+	size_t i;
+
+	i = 0;
+	while(*str++)
+		i++;
+	return (i);
+}
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char *str;
+	size_t i;
+	size_t len;
+
+	i = 0;
+	len = ft_strlen(s1) + ft_strlen(s2);
+	str = malloc(sizeof(char) * (len + 1));
+	while (*s1)
+		str[i++] = *s1++;
+	while (*s2)
+		str[i++] = *s2++;
+	str[++i] = '\0';
+	return (str);
+}
+
+char	*ft_strchr(const char *s, int c)
+{
+	size_t i;
+
+	i = 0;
+	while(s[i])
+	{
+		if (s[i] == (unsigned char)c)
+			return (&((char *)s)[i]);
+		i++;
+	}
+	return (NULL);
+}
+
+char	*ft_strdup(const char *s1)
+{
+	char	*s2;
+	int	len;
+	int	i;
+
+	len = ft_strlen(s1);
+	if (s1 == NULL)
+		return (NULL);
+	s2 = malloc(sizeof(char) * (len + 1));
+	if (s2 == NULL)
+		return (NULL);
+	i = 0;
+	while (s1[i])
+	{
+		s2[i] = s1[i];
+		i++;
+	}
+	s2[i] = '\0';
+	return (s2);
+}
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	char *s2;
+	unsigned int i;
+
+	i = 0;
+	if (!s)
+		return (NULL);
+	if (ft_strlen(s) < start)
+		len = 0;
+	s2 = malloc(sizeof(char) * (len + 1));
+	if (s2 == NULL)
+		return (NULL);
+	while (s[i] && i < len)
+	{
+		s2[i] = s[start + i];
+		i++;
+	}
+	s2[i] = '\0';
+	return (s2);
+}
