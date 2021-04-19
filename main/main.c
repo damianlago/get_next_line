29736e8bbc60b96ab2b1563bda0c7f296b981200
Damian Lago moran <dlago-mo@c1r8s5.42madrid.com>
2021-04-19 14:41:37 +0200

    update

--- main/main.c
+++ main/main.c
@@ -1,28 +1,32 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: dlago-mo <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/04/17 11:59:40 by dlago-mo          #+#    #+#             */
-/*   Updated: 2021/04/19 11:21:19 by dlago-mo         ###   ########.fr       */
+/*   Created: 2021/04/19 14:40:35 by dlago-mo          #+#    #+#             */
+/*   Updated: 2021/04/19 14:40:42 by dlago-mo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef GET_NEXT_LINE_H
-# define GET_NEXT_LINE_H
+#include "get_next_line.h"
+#include <stdio.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 
-# include <unistd.h>
-# include <stdio.h>
-# include <stdlib.h>
-# include <fcntl.h>
-
-size_t	ft_strlen(char *str);
-char	*ft_strjoin(char *s1, char *s2);
-char	*ft_strchr(char *s, int c);
-char	*ft_strdup(char *s1);
-char	*ft_substr(char *str, unsigned int start, size_t len);
-int		get_next_line(int fd, char **line);
-
-#endif
+int main(void)
+{
+	int fd;
+	char *line;
+	fd = open("test.txt", O_RDONLY);
+	while (get_next_line(fd, &line))
+	{
+		printf("%s", line);
+		free(line);
+		line = NULL;
+	}
+	
+	system("leaks a.out");
+	return (0);
+}
